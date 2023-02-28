0dd01ee7a9df8aee7d2824b3f68085c1c9d17242
ardelvito <ardelvito8738@gmail.com>
2023-02-28 17:01:25 +0700

    Testing First Commit

---
+++ app/src/main/java/com/example/anmp_c_160420030/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.example.anmp_c_160420030
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+            //todo testing git
+    }
+}
